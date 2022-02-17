6c4334306ef41193b49596461fa46906e653d50f
Kantor Marek (203667) <xkanto13@vutbr.cz>
2022-02-17 16:33:12 +0100

    2 reseni, kdyby byl caS - PRUMEROVALI BYCHOM
    Signed-off-by: Kantor Marek (203667) <xkanto13@vutbr.cz>

--- Radiologove/task2/filtrace.m
+++ Radiologove/task2/filtrace.m
@@ -42,3 +42,67 @@ imshow(A)
 
 %%
 res = noise_eval('C:\Users\xkanto13\Desktop\ABO2\path_save\', true);
+%% RESENI cislo 2 - podobny vysledek
+for i = 1:length(listing)
+    Obr = im2double(imread(['Lecture1_second_data\images_noise\', listing(i).name]));
+    
+    R = Obr(:,:,1);
+    G = Obr(:,:,2);
+    B = Obr(:,:,3);
+
+    R = imresize(R,0.25,'lanczos3','Antialiasing',true);
+    G = imresize(G,0.25,'lanczos3','Antialiasing',true);
+    B = imresize(B,0.25,'lanczos3','Antialiasing',true);
+
+%     figure;imshow(R,[])
+%     figure;imshow(G,[])
+%     figure;imshow(B,[])
+    
+    diff_R = imdiffusefilt(R);
+    diff_G = imdiffusefilt(G);
+    diff_B = imdiffusefilt(B);
+    
+%     figure;imshow(diff_R,[])
+%     figure;imshow(diff_G,[])
+%     figure;imshow(diff_B,[])
+
+    gauss_R = imgaussfilt(R,10,'FilterSize',25,'Padding','replicate','FilterDomain','frequency');
+    gauss_G = imgaussfilt(G,10,'FilterSize',25,'Padding','replicate','FilterDomain','frequency');
+    gauss_B = imgaussfilt(B,10,'FilterSize',25,'Padding','replicate','FilterDomain','frequency');
+     
+%     figure;imshow(gauss_R,[])
+%     figure;imshow(gauss_G,[])
+%     figure;imshow(gauss_B,[])
+    
+%     gauss_Obr = cat(3,gauss_R,gauss_G,gauss_B);
+
+    n=10;
+    med_R = medfilt2(R, [n n]);
+    med_G = medfilt2(G, [n n]);
+    med_B = medfilt2(B, [n n]);
+
+%     figure;imshow(med_R,[])
+%     figure;imshow(med_G,[])
+%     figure;imshow(med_B,[])
+    
+    med_Obr = cat(3,med_R,med_G,med_B);
+    
+    m=5;
+    wien_R = wiener2(R, [m m]);
+    wien_G = wiener2(G, [m m]);
+    wien_B = wiener2(B, [m m]);
+    wien_Obr = cat(3,wien_R,wien_G,wien_B);
+
+    R_f = imresize(imsharpen(((diff_R + gauss_R + med_R + wien_R)./4)), 4,'lanczos3','Antialiasing',true);
+    G_f = imresize(imsharpen(((diff_G + gauss_G + med_G + wien_G)./4)), 4,'lanczos3','Antialiasing',true);
+    B_f = imresize(imsharpen(((diff_B + gauss_B + med_B + wien_B)./4)), 4,'lanczos3','Antialiasing',true);
+
+    Obr_f = cat(3,R_f,G_f,B_f);
+
+    Obr_f = uint8(im2double(Obr_f).*255);
+    
+%     figure;
+%     montage({Obr,Obr_f},'ThumbnailSize',[])
+%     title('Original (left) vs. Restored (Right)')
+    imwrite(Obr_f,['V:/MPC_AB2/1/task2/Lecture1_second_data/images_denoise/' listing(i).name])
+end
